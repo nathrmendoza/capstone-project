1c4d869d2dd16682dd9cd721ffc28522cfc23580
Nath <mendoza.nathaniel328@gmail.com>
2022-06-27 13:05:59 +0800

    finish add to cart

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
+import '../styles/components/cart-item.scss'
+
 const CartItem = ({item}) => {
-  const {name, quantity} = item;
+  const {name, imageUrl, quantity, price} = item;
   
   return (
     <div className='cart-item'>
-      <h5>{name}</h5>
-      <span className='quantity'>x{quantity}</span>
+      <aside>
+        <img src={imageUrl} alt={`${name}-pic`}/>
+      </aside>
+      <article>
+        <h5>{name}</h5>
+        <div className='quan-price'>
+          <span className='quantity'>x{quantity}</span>
+          <span className='price'>${price}</span>
+        </div>
+      </article>
     </div>
   )
 }
